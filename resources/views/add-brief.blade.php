e70e1114b0554af4276e4f7ba35c9dd0416c6b74
Chelloutt <92926704+Chelloutt@users.noreply.github.com>
2022-11-07 17:01:27 +0100

    first commit

---
+++ resources/views/add-brief.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ajouter brief</title>
+</head>
+<body>
+    <center>
+        <h3>ajouter brief</h3><br><br>
+
+        
+<form action=>
+    nom de brief:<br> <input type="text"><br>
+    date/heure de livraison:<br> <input type="text"><br>
+    date/heure de récupération:<br> <input type="text"><br>
+
+    <input type="button" value="ajouter">
+</form>
+    </center>
+</body>
+</html>
